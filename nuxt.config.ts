71c684a2072e34df778d437a31c432434e270e24
sergio <sergio9929@hotmail.com>
2025-03-16 20:40:36 +0100

    update all dependencies to the latest version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    modules: ['@vueuse/nuxt', '@formkit/auto-animate/nuxt'],
-    css: ['~/assets/style.css'],
-    imports: {
-        dirs: ['types/*.ts'],
-    },
+  modules: ['@vueuse/nuxt', '@formkit/auto-animate/nuxt'],
+  css: ['~/assets/style.css'],
+
+  imports: {
+      dirs: ['types/**'],
+  },
+
+  compatibilityDate: '2025-03-16',
 })
